bbaed53df63a358a8f0ee7a2a344b73c51177850
Simon Gonzalez <ximonali@gmail.com>
2016-12-06 18:12:00 -0500

    test1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var app = express();
-var port = 8080;
+var port = process.env.PORT || 5000;
 
 //use ejs and express Layouts
 app.set('view engine','ejs');
